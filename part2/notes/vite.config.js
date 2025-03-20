d81a26573077bf0bced08b2437ecdbb68f7f99c4
Sandesh Bhattarai <sandeshbhattarai@Sandeshs-MacBook-Pro.local>
2025-03-20 13:52:15 -0500

    Made backend and frontend work together

--- part2/notes/vite.config.js
+++ part2/notes/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: process.env.PORT || 5173, // Use the provided PORT
+  },
   plugins: [react()],
 })

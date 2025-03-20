18457bf82538ab3d26620190f181ba2e6bd6a78f
Sandesh Bhattarai <sandeshbhattarai@Sandeshs-MacBook-Pro.local>
2025-03-20 12:52:17 -0500

    Final Commit

---
+++ part2/backend3/index.js
@@ -0,0 +1,85 @@
+const express = require("express")
+
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+// Customized middleware;
+
+
+
+const requestLogger = (req,res,next) =>{
+    console.log('method:', req.method)
+    console.log('path:', req.path)
+    console.log('body:', req.body)
+    console.log('----')
+    next()
+}
+
+app.use(requestLogger)
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+
+app.get('/api/notes/:id', (req,res) => {
+    const id =request.params.id;
+    const note = notes.find(note => note.id === id)
+    res.json(note)
+})
+app.delete('/api/notes/:id', (req,res) => {
+    const id = req.params.id
+    notes = notes.filter(note => note.id !== id )
+    res.status(204).end()
+})
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+app.post('/api/notes', (req,res) => {
+    const body = req.body
+    if(!body.content){
+        return res.status(400).json({
+            error: 'missing content'
+        })
+    }
+
+    const note = {
+        content: body.content, 
+        important: body.important || false,
+        id: generateId(),
+    }
+    notes = notes.concat(note)
+    response.json(note)
+
+})
+
+const unknownEndpoint = (req,res) => {
+    res.status(404).send({error : "unknown endpoint"})
+}
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import Footer from './components/Footer'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -21,26 +23,34 @@ const App = () => {
       important: Math.random() > 0.5,
     }
 
-    noteService.create(noteObject).then((returnedNotes) => {
-      setNotes(notes.concat(returnedNotes))
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote))
       setNewNote('')
     })
   }
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
-  }
-  const toggleImportantOf = (id) => {
-
+  const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
-    noteService.update(id, changedNote).then((returnedNotes) => {
-      setNotes(notes.map(n => n.id !== id ? n : returnedNotes))
-    }).catch(error => {
-      alert(`the note '${note.content}' was already deleted from server`)
-      setNotes(notes.filter(n => n.id !== id))
-    })
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter((n) => n.id !== id))
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
   }
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
@@ -48,6 +58,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -55,13 +66,18 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} toggleEvent={() => toggleImportantOf(note.id)} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
